eec7553a77d0a5dc1155e51e765ed05975e0929a
Lotte Bransen <l.bransen@scisports.com>
2024-06-06 11:24:08 +0200

    Add password check at start

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
+
+import hmac
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import datetime
 import pandas as pd
 
 st.set_page_config(layout='wide')
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Wachtwoord", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Verkeerd wachtwoord")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 st.title('Amelisweerdcup 2024')
 
 st.logo('amelisweerdcup_logo.png')
